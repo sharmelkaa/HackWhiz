e87c3e0469de777cf2127b76ca2b88c7376b05b9
Юлия Прокофьева <sharmelka@ITgirl.local>
2024-01-17 18:23:13 +0500

    Linter fixed

--- frontend/src/pages/publications/components/Publication/index.jsx
+++ frontend/src/pages/publications/components/Publication/index.jsx
@@ -1,22 +1,23 @@
-import {useParams} from "react-router";
-import {useDispatch, useSelector} from "react-redux";
-import {useState} from "react";
+import { useParams } from 'react-router'
+import { useDispatch, useSelector } from 'react-redux'
+import { useState } from 'react'
 import * as SC from './styles'
-import {updateUser} from "../../../../slices/userSlice";
-import {Modal} from "../../../../components/UI/Modal";
-import {fetchData} from "../../../../api/fetchData";
-import {EditPublicationForm} from "./components/EditPublicationForm";
-import {WarningModal} from "../../../../components/UI/WarningModal";
-import {PublicationContent} from "./components/PublicationContent";
+import { updateUser } from '../../../../slices/userSlice'
+import { Modal } from '../../../../components/UI/Modal'
+import { fetchData } from '../../../../api/fetchData'
+import { EditPublicationForm } from './components/EditPublicationForm'
+import { WarningModal } from '../../../../components/UI/WarningModal'
+import { PublicationContent } from './components/PublicationContent'
 
 const DELETE_WARNING = 'Are you sure you want to delete this publication?'
-export const Publication = ({ post, access, setPublications, publications }) => {
+export function Publication({ post, access, setPublications, publications }) {
     const postID = post._id
     const [modalMessage, setModalMessage] = useState(null)
     const [warningModal, setWarningModal] = useState(null)
     const dispatch = useDispatch()
     const [editMode, setEditMode] = useState(false)
     const { isAdmin } = useSelector((state) => state.user)
+
     const { username } = useParams()
     const onOpenModal = (message) => {
         setModalMessage(message)
@@ -36,20 +37,26 @@ export const Publication = ({ post, access, setPublications, publications }) => 
     const onCloseWarningModal = () => {
         setWarningModal(null)
     }
-    const deletePost = async() => {
+    const deletePost = async () => {
         if (isAdmin) {
-            const admin_response = await fetchData('admin_delete_post', 'DELETE', {postID, username})
-            if (admin_response.hasOwnProperty('message')) {
-                setModalMessage(admin_response.message)
+            const adminResponse = await fetchData(
+                'admin_delete_post',
+                'DELETE',
+                { postID, username }
+            )
+            if (adminResponse.hasOwnProperty('message')) {
+                setModalMessage(adminResponse.message)
                 return
             }
-            const index = publications.map((publication) => publication._id).indexOf(postID)
+            const index = publications
+                .map((publication) => publication._id)
+                .indexOf(postID)
             publications.splice(index, 1)
             setPublications(publications)
             return
         }
 
-        const response = await fetchData('delete_post', 'DELETE',  {postID})
+        const response = await fetchData('delete_post', 'DELETE', { postID })
         if (response.hasOwnProperty('message')) {
             setModalMessage(response.message)
             return
@@ -57,36 +64,33 @@ export const Publication = ({ post, access, setPublications, publications }) => 
         dispatch(updateUser(response))
     }
 
-    return(
+    return (
         <SC.Wrapper>
-            {modalMessage &&
-                <Modal
-                    text={modalMessage}
-                    onClose={onCloseModal}
-                />
-            }
-            {warningModal &&
+            {modalMessage && (
+                <Modal text={modalMessage} onClose={onCloseModal} />
+            )}
+            {warningModal && (
                 <WarningModal
                     text={warningModal}
                     onClose={onCloseWarningModal}
                     onYes={deletePost}
                 />
-            }
-            {!editMode &&
+            )}
+            {!editMode && (
                 <PublicationContent
                     post={post}
                     access={access}
                     onOpenWarningModal={onOpenWarningModal}
                     openEditForm={openEditForm}
                 />
-            }
-            {editMode &&
-                    <EditPublicationForm
-                        post={post}
-                        onOpenModal={onOpenModal}
-                        closeEditForm={closeEditForm}
-                    />
-            }
+            )}
+            {editMode && (
+                <EditPublicationForm
+                    post={post}
+                    onOpenModal={onOpenModal}
+                    closeEditForm={closeEditForm}
+                />
+            )}
         </SC.Wrapper>
     )
 }
